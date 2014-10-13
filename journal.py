b17ec864bd1a5fd0b4cd432358d25f794f3326f5
Charles Gust <charlesg@equality.net>
2014-10-13 11:54:47 -0700

    add Step1 with initial Procfile and journal module

---
+++ journal.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+import os
+import psycopg2
+from contextlib import closing
+
+DB_SCHEMA = """
+DROP TABLE IF EXISTS entries;
+CREATE TABLE entries (
+    id serial PRIMARY KEY,
+    title VARCHAR (127) NOT NULL,
+    text TEXT NOT NULL,
+    created TIMESTAMP NOT NULL
+);
+"""
+
+app = Flask(__name__)
+app.config['DATABASE'] = os.environ.get(
+    'DATABASE_URL', 'dbname=learning_journal user=charlesgust'
+    )
+
+
+def connect_db():
+    """ return a connection to the configured databse """
+    return psycopg2.connect(app.config['DATABASE'])
+
+
+def init_db():
+    """ initialize the dabatase using DB_SCHEMA
+
+    WARNING: executing this function will drop existing tables
+    """
+    with closing(connect_db()) as db:
+        db.cursor().execute(DB_SCHEMA)
+        db.commit()
+
+
+@app.route('/')
+def hello():
+    return u'Hello world!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
